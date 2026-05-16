16f4a34846c3401024a310c15adcbb80eee817d1
Benjamin940 <gat563@altenforst.de>
2026-05-16 12:06:14 +0200

    Füge Information über RSS-Feed hinzu

---
+++ pages/bleib-aktuell/index.js
@@ -0,0 +1,23 @@
+export default function Bleib_aktuellIndex() {
+  return (
+    <main>
+      <section className="archiv">
+        <h1>Blein am Laufenden</h1>
+        <p>Hier erfärst du, wie du keinen Artikel mehr verpasst.</p>
+        <ol>
+          <li>
+            <b>RSS-Feed: </b>
+            Du kannst diesen Link in einen kompatiblen RSS-Reader(z.B.:{" "}
+            <a href="https://www.feedflow.dev">FeedFlow</a>) einfügen um über
+            neue Artikel informiert zu werden. <br></br>
+            <code>https://wegatnews.de/feed.xml</code>
+          </li>
+          <li>
+            <b>E-Mail Newsletter: </b>
+            Coming soon
+          </li>
+        </ol>
+      </section>
+    </main>
+  );
+}
