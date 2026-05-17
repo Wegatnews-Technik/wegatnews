01fb426b2340e070f9b5d740375aaf8f1814acf5
Benjamin940 <gat563@altenforst.de>
2026-05-17 19:05:50 +0200

    Tag-Schema vereinheitlichen, funktion getPostByTag hinzufügen

--- pages/informativ/index.js
+++ pages/informativ/index.js
@@ -1,10 +1,24 @@
-export default function InformativIndex() {
+import { getPostsByTag } from "../../lib/posts";
+
+export async function getStaticProps() {
+  const informativ_posts = getPostsByTag("informativ");
+
+  return {
+    props: {
+      posts: informativ_posts,
+    },
+  };
+}
+
+export default function InformativIndex({ posts }) {
   return (
     <main>
       <section className="archiv">
         <h1>Informativ 📖</h1>
         <p>Berichte, Argumentationen, Stellungnahmen. Sachliche Diskussionen</p>
-        <p><b>Coming soon</b></p>
+        <p>
+          <b>{posts}</b>
+        </p>
       </section>
     </main>
   );
