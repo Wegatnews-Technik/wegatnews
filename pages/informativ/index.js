2c9787925f505d4800d82bc4b7162bfdd7410d08
Benjamin940 <gat563@altenforst.de>
2026-05-17 19:40:52 +0200

    Zeige Informative Posts unter /informativ

--- pages/informativ/index.js
+++ pages/informativ/index.js
@@ -1,7 +1,8 @@
 import { getPostsByTag } from "../../lib/posts";
+import PostGrid from "../../components/blog/PostGrid";
 
 export async function getStaticProps() {
-  const informativ_posts = getPostsByTag("informativ");
+  const informativ_posts = getPostsByTag("Informativ");
 
   return {
     props: {
@@ -10,15 +11,33 @@ export async function getStaticProps() {
   };
 }
 
+function chunkPosts(posts, size) {
+  const chunks = [];
+
+  for (let index = 0; index < posts.length; index += size) {
+    chunks.push(posts.slice(index, index + size));
+  }
+
+  return chunks;
+}
+
+function NewestPosts({ posts }) {
+  return (
+    <section className="blog-section">
+      {chunkPosts(posts, 4).map((group, index) => (
+        <PostGrid key={index} posts={group} />
+      ))}
+    </section>
+  );
+}
+
 export default function InformativIndex({ posts }) {
   return (
     <main>
       <section className="archiv">
         <h1>Informativ 📖</h1>
         <p>Berichte, Argumentationen, Stellungnahmen. Sachliche Diskussionen</p>
-        <p>
-          <b>{posts}</b>
-        </p>
+        <NewestPosts posts={posts} />
       </section>
     </main>
   );
