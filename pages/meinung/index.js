7126e9943484458e49c474c53ffedc992b486310
Benjamin940 <gat563@altenforst.de>
2026-05-17 20:05:17 +0200

    Posts für (/meinung und /umfragen hinzufügen

--- pages/meinung/index.js
+++ pages/meinung/index.js
@@ -1,10 +1,43 @@
-export default function InformativIndex() {
+import { getPostsByTag } from "../../lib/posts";
+import PostGrid from "../../components/blog/PostGrid";
+
+export async function getStaticProps() {
+  const meinung_posts = getPostsByTag("Meinung");
+
+  return {
+    props: {
+      posts: meinung_posts,
+    },
+  };
+}
+
+function chunkPosts(posts, size) {
+  const chunks = [];
+
+  for (let index = 0; index < posts.length; index += size) {
+    chunks.push(posts.slice(index, index + size));
+  }
+
+  return chunks;
+}
+
+function NewestPosts({ posts }) {
+  return (
+    <section className="blog-section">
+      {chunkPosts(posts, 4).map((group, index) => (
+        <PostGrid key={index} posts={group} />
+      ))}
+    </section>
+  );
+}
+
+export default function InformativIndex({ posts }) {
   return (
     <main>
       <section className="archiv">
         <h1>Meinung 💣</h1>
         <p>Eigene Ansichten, provokative Diskussionen, Satire</p>
-        <p><b>Coming soon</b></p>
+        <NewestPosts posts={posts} />
       </section>
     </main>
   );
--- pages/umfragen/index.js
+++ pages/umfragen/index.js
@@ -1,10 +1,43 @@
-export default function InformativIndex() {
+import { getPostsByTag } from "../../lib/posts";
+import PostGrid from "../../components/blog/PostGrid";
+
+export async function getStaticProps() {
+  const umfragen_posts = getPostsByTag("Umfrage");
+
+  return {
+    props: {
+      posts: umfragen_posts,
+    },
+  };
+}
+
+function chunkPosts(posts, size) {
+  const chunks = [];
+
+  for (let index = 0; index < posts.length; index += size) {
+    chunks.push(posts.slice(index, index + size));
+  }
+
+  return chunks;
+}
+
+function NewestPosts({ posts }) {
+  return (
+    <section className="blog-section">
+      {chunkPosts(posts, 4).map((group, index) => (
+        <PostGrid key={index} posts={group} />
+      ))}
+    </section>
+  );
+}
+
+export default function InformativIndex({ posts }) {
   return (
     <main>
       <section className="archiv">
         <h1>Umfragen 🎤</h1>
         <p>Interessante Umfragen zu aktuellen Themen</p>
-        <p><b>Coming soon</b></p>
+        <NewestPosts posts={posts} />
       </section>
     </main>
   );
