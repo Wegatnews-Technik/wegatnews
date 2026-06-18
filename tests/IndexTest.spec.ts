5cdac1f41a448b32833944a3691f5defa0c6362a
Benjamin940 <gat563@altenforst.de>
2026-06-18 20:40:03 +0200

    Tests für alle Navbar-links

--- tests/IndexTest.spec.ts
+++ tests/IndexTest.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { getAllPosts } from "../lib/posts";
 
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:3000/');
@@ -7,12 +8,32 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle("WE G(A)T NEWS – Die Schülerzeitung am Altenforst");
 });
 
-test('Informativ link', async ({ page }) => {
+test("Navbar links", async ({ page }) => {
   await page.goto('http://localhost:3000/');
 
   // Click the get started link.
   await page.getByRole('navigation').getByRole('link', { name: 'Informativ📖' }).click();
-
-  // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Informativ 📖' })).toBeVisible();
+
+  await page.getByRole('navigation').getByRole('link', { name: 'Meinung💣' }).click();
+  await expect(page.getByRole('heading', { name: 'Meinung 💣' })).toBeVisible();
+
+  await page.getByRole('navigation').getByRole('link', { name: 'Umfragen🎤' }).click();
+  await expect(page.getByRole('heading', { name: 'Umfragen 🎤' })).toBeVisible();
+
+  await page.getByRole('navigation').getByRole('link', { name: 'Buchrezensionen📕' }).click();
+  await expect(page.getByRole('heading', { name: 'Buchrezensionen 📕' })).toBeVisible();
+
+  await page.getByRole('navigation').getByRole('link', { name: 'Mach mit👋' }).click();
+  await expect(page.getByRole('heading', { name: 'Ideen, Artikel, Probleme, Fragen?' })).toBeVisible();
 });
+
+// test("Tags", async ({ page }) => {
+//   await page.goto('http://localhost:3000/');
+
+//   let newestPost = getAllPosts()[0];
+//   console.log(newestPost);
+
+//   await page.getByRole("link", { name: newestPost.tag }).click();
+//   await expect(page.getByText(newestPost.tags)).toBeVisible();
+// });
