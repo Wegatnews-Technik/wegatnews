16bed13c28a6dd41a438eff0c2e6ff2ddb0e0161
Chenjv2 <chenjv2.vcbs@gmail.com>
2026-08-05 11:13:00 +0200

    ueberarbeitung der ueber uns seite, jetzt muss nur noch Ouijdan etwas dazu schreiben

--- pages/ueber-uns/index.js
+++ pages/ueber-uns/index.js
@@ -2,47 +2,65 @@ export default function UeberUnsIndex() {
   return (
     <main>
       <section>
+        <h1>Über uns</h1>
         <div className="container">
           <div className="image-or-text">
             <h2>Gründung</h2>
             <p>
-              Die Schülerzeitung wurde von Ouijdan und Vincent gegründet,
-              um den Schülern eine Plattform zu bieten, auf der sie interessante Artikel und Meinungen veröffentlichen können.
+              Die Schülerzeitung, eine Idee von Ouijdan, wurde gegründet, um den
+              Schülern eine Plattform zu bieten, auf der sie interessante
+              Artikel und Meinungen veröffentlichen können. Unser Ziel ist es...
+            </p>
+          </div>
+          <div className="image-or-text">
+            <h2>Verantwortliche</h2>
+            <p>
+              <b>Inhaltliche Verantwortliche:</b> Ouijdan
+              <br />
+              <b>Technischer Verantwortliche:</b> Vincent
+              <br />
+              <b>Stv. technischer Verantwortliche:</b> Benjamin
             </p>
           </div>
           <div className="image-or-text">
             <h2>Team</h2>
-            <h3>Redaktion</h3>
-            <ul>
-              <li>Ouijdan</li>
-              <li>Vincent</li>
-              <li>Maya</li>
-              <li>Maja</li>
-              <li>Matilda</li>
-              <li>Karlotta</li>
-              <li>Joel</li>
-              <li>Danielle</li>
-              <li>Sophie</li>
-              <li>Charlotte</li>
-              <li>Nori</li>
-              <li>Kati</li>
-              <li>Meryem</li>
-              <li>Valeriia</li>
-              <li>Amaya</li>
-              <li></li>
-            </ul>
-            <h3>Technik</h3>
-            <ul>
-              <li>Vincent</li>
-              <li>Benjamin</li>
-            </ul>
+            <p>
+              Amaya
+              <br /> Aurelia
+              <br /> Benjamin
+              <br /> Charlotte
+              <br /> Clara
+              <br /> Danielle
+              <br /> Ibrahim
+              <br /> Jason
+              <br /> Karlotta
+              <br /> Maja
+              <br /> Matilda
+              <br /> Melanie
+              <br /> Meryem
+              <br /> Nori
+              <br /> Ouijdan
+              <br /> Philipp
+              <br /> Sophie
+              <br /> Valeriia
+              <br /> Vincent
+              <br /> Vincent
+              <br /> Vincent
+              <br /> Wyn
+              <br /> Zeynep
+            </p>
           </div>
           <div className="image-or-text">
-            <h2>Wie du dich beteiligen kannst</h2>
+            <h2>Lehrer</h2>
             <p>
-              Wir treffen uns jeden Donnerstag in der Mittagspause vor den Computerräumen.
-              Da könnt ihr gerne vorbeikommen, wenn ihr auch mithelfen wollt.
-              Außerdem könnt ihr auch gerne eine E-Mail schreiben: <a href="mailto:wegatnews@outlook.de">wegatnews@outlook.de</a>
+              Natürlich versuchen wir so unabhängig wie möglich zu arbeiten,
+              dennoch haben und einige Lehrkäfte so tatkräftig unterstützt, dass
+              sie auch fest zum Team gehören.{" "}
+            </p>
+            <p>
+              Herr Pollheim
+              <br />
+              Frau Cramer
             </p>
           </div>
         </div>
